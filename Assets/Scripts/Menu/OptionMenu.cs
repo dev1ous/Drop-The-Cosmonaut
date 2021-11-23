e05093bac06bb80cdfb126762bfa0c125f9db81e
Tamaho <52009799+Tama-sama@users.noreply.github.com>
2021-11-23 10:26:43 +0100

    Fix player stuck, feedback vibration & save and load settings

--- Assets/Scripts/Menu/OptionMenu.cs
+++ Assets/Scripts/Menu/OptionMenu.cs
@@ -5,17 +5,38 @@ using UnityEngine.UI;
 
 public class OptionMenu : MonoBehaviour
 {
+    public class Settings
+    {
+        public float volume = 0f;
+        public bool vibrationEnabled = false;
+        public bool gyroEnabled = false;
+    }
+
     [SerializeField] private Button touchButton = null;
     [SerializeField] private Button gyroButton = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private Toggle vibrationToggle = null;
 
     private Image touchImage = null;
     private Image gyroImage = null;
 
+    public static Settings settings = new Settings();
+
     void Start()
     {
+        settings.volume = PlayerPrefs.GetFloat("Volume");
+        settings.gyroEnabled = System.Convert.ToBoolean(PlayerPrefs.GetInt("GyroEnabled"));
+        settings.vibrationEnabled = System.Convert.ToBoolean(PlayerPrefs.GetInt("VibrationEnabled"));
+
+
         if (SystemInfo.supportsGyroscope == false)
         {
             gyroButton.interactable = false;
+            settings.gyroEnabled = false;
+        }
+        else
+        {
+            Input.gyro.enabled = settings.gyroEnabled;
         }
 
         touchImage = touchButton.GetComponent<Image>();
@@ -34,6 +55,11 @@ public class OptionMenu : MonoBehaviour
             touchImage.color = Color.cyan;
             gyroImage.color = Color.white;
         }
+
+        vibrationToggle.isOn = settings.vibrationEnabled;
+        volumeSlider.value = settings.volume;
+
+        settings.gyroEnabled = Input.gyro.enabled;
     }
 
     public void ClickTouch()
@@ -44,5 +70,22 @@ public class OptionMenu : MonoBehaviour
     public void ClickGyro()
     {
         Input.gyro.enabled = true;
+    }
+
+    public void ChangeVolume()
+    {
+        settings.volume = volumeSlider.value;
+    }
+
+    public void ClickVibration()
+    {
+        settings.vibrationEnabled = vibrationToggle.isOn;
+    }
+
+    public static void Save()
+    {
+        PlayerPrefs.SetFloat("Volume", settings.volume);
+        PlayerPrefs.SetInt("GyroEnabled", System.Convert.ToInt32(settings.gyroEnabled));
+        PlayerPrefs.SetInt("VibrationEnabled", System.Convert.ToInt32(settings.vibrationEnabled));
     }
 }
