511e4d50206bee68186e5cfa60af4916e1b06e37
Tamaho <Tamaho@vivaldi.net>
2021-11-29 21:21:51 +0100

    MainMenu background minimum animation

--- Assets/Scripts/Menu/MenuCamera.cs
+++ Assets/Scripts/Menu/MenuCamera.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class MenuCamera : MonoBehaviour
 {
     public enum Lookat { MAINMENU, OPTION, CREDIT }
-    
+
     [SerializeField] private Camera usedCamera = null;
     private Lookat lookat = Lookat.MAINMENU;
+
+    [SerializeField] private GameObject[] obstacles = null;
+    private int usedIndex = -1;
+    private float lerpTimer;
+    private float randomY = 0f;
+    private float randomDelay = 0f;
 
     void Update()
     {
@@ -22,6 +28,26 @@ public class MenuCamera : MonoBehaviour
         else if (lookat == Lookat.CREDIT)
         {
             usedCamera.transform.rotation = Quaternion.Euler(0f, Mathf.MoveTowardsAngle(usedCamera.transform.rotation.eulerAngles.y, -90, 180f * Time.deltaTime), 0f);
+        }
+
+        if (usedIndex == -1 && obstacles.Length != 0)
+        {
+            randomY = Random.Range(-300f, 300f);
+            usedIndex = Random.Range(0, obstacles.Length);
+            lerpTimer = 0f;
+            randomDelay = Random.Range(0f, 0.5f);
+        }
+        else
+        {
+            lerpTimer += Time.deltaTime / 4.5f;
+            obstacles[usedIndex].transform.localPosition = Vector3.Lerp(new Vector3(275, randomY, 550), new Vector3(-275, randomY, 550), lerpTimer);
+            obstacles[usedIndex].transform.Rotate(new Vector3(30f, 30f, 30f) * Time.deltaTime);
+
+            if (lerpTimer >= 1f + randomDelay)
+            {
+                obstacles[usedIndex].transform.localPosition = new Vector3(0f, 0f, -100f);
+                usedIndex = -1;
+            }
         }
     }
 
